32c912927d7b696c6d33013f00f9a0af04a92539
JuninhoSouza25 <dnt-bh@hotmail.com>
2021-12-20 23:34:59 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let gallery = document.getElementById("gallery");
+let logo = document.getElementById("lg-3");
+
+logo.addEventListener('click', function showGallery(){
+
+    gallery.classList.toggle("off")
+})
